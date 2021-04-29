898a195c017d18c40e31a51ddf0188f6d5bb381c
guillaume <guillaumedemange@free.fr>
2021-04-29 20:25:19 +0200

    fix errors

--- setup.py
+++ setup.py
@@ -62,5 +62,5 @@ if __name__ == "__main__":
               'Operating System :: MacOS'
             ],
           install_requires=["numpy", "scipy", "scikit-learn>=0.18", "cython"],
-          setup_requires=["cython"],
-          ext_modules=extensions)
+          setup_requires=["cython"]
+         )
